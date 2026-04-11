05412abd6cb0aee37aff9b56ef0636c9bcef6f41
PikluInduDhara <dhara.lok93@gmail.com>
2026-04-11 21:02:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ import base64
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 
-# ✅ FIX FOR STREAMLIT CLOUD (RUN ONCE)
-if os.path.exists("store.db"):
-    os.remove("store.db")
-
 
 st.set_page_config(page_title="Sajai Tomay", layout="wide")
 
